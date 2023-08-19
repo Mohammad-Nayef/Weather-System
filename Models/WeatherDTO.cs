97cf14e1d4ed5fddb99386d8d8a6bbdc0609a75f
mohammad-nayef <awladmohammad20@gmail.com>
2023-08-19 23:59:24 +0300

    Added WeatherDTO class

---
+++ Models/WeatherDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace WeatherSystem.Models
+{
+    public class WeatherDTO
+    {
+        public string Location { get; set; }
+        public float Temperature { get; set; }
+        public float Humidity { get; set; }
+        
+        public WeatherDTO(string location, float temperature, float humidity)
+        {
+            Location = location;
+            Temperature = temperature;
+            Humidity = humidity;
+        }
+    }
+}
