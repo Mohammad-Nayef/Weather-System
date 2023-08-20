c91fe94226ef9c241cfd1ddad5a1bb2ad1f38bca
mohammad-nayef <awladmohammad20@gmail.com>
2023-08-20 09:42:12 +0300

    XmlInputStrategy class added as a concrete strategy

---
+++ XmlInputStrategy.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml.Serialization;
+using WeatherSystem.Models;
+
+namespace WeatherSystem
+{
+    public class XmlInputStrategy : IInputStrategy
+    {
+        public WeatherDTO? GetWeatherDTO(string input)
+        {
+            var weather = new WeatherDTO();
+            var serializer = new XmlSerializer(weather.GetType());
+
+            using (var reader = new StringReader(input))
+            {
+                weather = (WeatherDTO?)serializer.Deserialize(reader);
+            }
+
+            return weather;
+        }
+    }
+}
