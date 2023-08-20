1badeb750cb0877392916d12a83a385ad2feba05
mohammad-nayef <awladmohammad20@gmail.com>
2023-08-20 10:22:13 +0300

    Added Context class that connects the client with strategies

---
+++ Context.cs
@@ -0,0 +1,21 @@
+﻿using WeatherSystem.Models;
+
+namespace WeatherSystem
+{
+    public  class Context
+    {
+        private IInputStrategy _strategy;
+
+        public Context(IInputStrategy strategy)
+        {
+            _strategy = strategy;
+        }
+
+        public void SetStrategy(IInputStrategy strategy)
+        {
+            _strategy = strategy;
+        }
+
+        public WeatherDTO WeatherDeserialize(string input) => _strategy.GetWeatherDTO(input);        
+    }
+}
