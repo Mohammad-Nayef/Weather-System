c8ea4fe81809f12dc23b6dcbf573cc8a30522664
mohammad-nayef <awladmohammad20@gmail.com>
2023-08-20 22:36:38 +0300

    Added ConfigRepository as Singleton class

---
+++ Repositories/ConfigRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace WeatherSystem.Repositories
+{
+    public class ConfigRepository
+    {
+        private static ConfigRepository? _instance = null;
+        private const string _configFilePath = "Configs\\BotsConfig.json";
+
+        private ConfigRepository() { }
+
+
+    }
+}
