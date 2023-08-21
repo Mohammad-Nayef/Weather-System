4de94b08fa8f03b38d4a6901f82d6520cf73d067
mohammad-nayef <awladmohammad20@gmail.com>
2023-08-21 18:07:06 +0300

    Impelemented the method that updates the threshold

--- Models/WeatherBotDTO.cs
+++ Models/WeatherBotDTO.cs
@@ -7,7 +7,15 @@
         public string? Message { get; set; }
         public int? HumidityThreshold { get; set; }
         public int? TemperatureThreshold { get; set; }
+        public bool? IsThresholdTriggered { get; private set; } = false;
 
-        //public void Update()
+        public void Update(WeatherStateDTO weatherState)
+        {
+            if (HumidityThreshold != null)
+                IsThresholdTriggered = HumidityThreshold <= weatherState.Humidity;
+
+            if (TemperatureThreshold != null)
+                IsThresholdTriggered = TemperatureThreshold <= weatherState.Temperature;
+        }
     }
 }
