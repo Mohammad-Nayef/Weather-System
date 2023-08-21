f1c60168718c54584e9af52963f59224dd9859ca
mohammad-nayef <awladmohammad20@gmail.com>
2023-08-21 15:33:30 +0300

    Changed bots architecture

--- WeatherBot.cs
+++ WeatherBot.cs
@@ -1,12 +1,15 @@
 ﻿namespace WeatherSystem
 {
-    public abstract class IWeatherBot
+    public class WeatherBot
     {
         public bool? Enabled { get; set; }
         public string? Message { get; set; }
         public int? HumidityThreshold { get; set; }
         public int? TemperatureThreshold { get; set; }
 
-        public abstract void Update();
+        public void Update()
+        {
+
+        }
     }
 }
