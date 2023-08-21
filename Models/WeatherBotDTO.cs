0e3f10ee3f3e8ba87e4e25bb2e4c8129236aae43
mohammad-nayef <awladmohammad20@gmail.com>
2023-08-21 18:23:02 +0300

    Added a property that makes more sense

--- Models/WeatherBotDTO.cs
+++ Models/WeatherBotDTO.cs
@@ -6,16 +6,39 @@
         public bool? Enabled { get; set; }
         public string? Message { get; set; }
         public int? HumidityThreshold { get; set; }
-        public int? TemperatureThreshold { get; set; }
+        public int? TemperatureAboveThreshold { get; set; }
+        public int? TemperatureUnderThreshold { get; set; }
         public bool? IsThresholdTriggered { get; private set; } = false;
 
         public void Update(WeatherStateDTO weatherState)
         {
-            if (HumidityThreshold != null)
-                IsThresholdTriggered = HumidityThreshold <= weatherState.Humidity;
+            IsThresholdTriggered = HumidityThresholdTriggered(weatherState) ||
+                TemperatureAboveThresholdTriggered(weatherState) ||
+                TemperatureUnderThresholdTriggered(weatherState);
+        }
 
-            if (TemperatureThreshold != null)
-                IsThresholdTriggered = TemperatureThreshold <= weatherState.Temperature;
+        private bool HumidityThresholdTriggered(WeatherStateDTO weatherState)
+        {
+            if (HumidityThreshold != null)
+                return HumidityThreshold <= weatherState.Humidity;
+
+            return false;
+        }
+
+        private bool TemperatureAboveThresholdTriggered(WeatherStateDTO weatherState)
+        {
+            if (TemperatureAboveThreshold != null)
+                return TemperatureAboveThreshold < weatherState.Temperature;
+
+            return false;
+        }
+
+        private bool TemperatureUnderThresholdTriggered(WeatherStateDTO weatherState)
+        {
+            if (TemperatureUnderThreshold != null)
+                return TemperatureUnderThreshold > weatherState.Temperature;
+
+            return false;
         }
     }
 }
