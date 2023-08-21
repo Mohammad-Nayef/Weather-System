aa9d5b138837af69f25ece3fc75eeb916f96cc9b
mohammad-nayef <awladmohammad20@gmail.com>
2023-08-21 17:04:06 +0300

    Added BotsObserver class to notify the subscribed bots

---
+++ BotsObserver.cs
@@ -0,0 +1,17 @@
+﻿using WeatherSystem.Models;
+using WeatherSystem.Services;
+
+namespace WeatherSystem
+{
+    public static class BotsObserver
+    {
+        public static List<WeatherBotDTO> Subscribers {
+            get => BotsService.EnabledWeatherBots;
+        }
+
+        public static void NotifySubscribers(WeatherStateDTO weatherState)
+        {
+            Subscribers.ForEach(subscriber => subscriber.Update(weatherState));
+        }
+    }
+}
