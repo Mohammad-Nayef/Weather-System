9863a595b7ea653e1a68f04477a2a4e7f0ae3280
mohammad-nayef <awladmohammad20@gmail.com>
2023-08-21 18:07:54 +0300

    Implemented the console info

--- Program.cs
+++ Program.cs
@@ -1,13 +1,55 @@
 ﻿using WeatherSystem;
-using WeatherSystem.Repositories;
-using System.Text.Json;
+using WeatherSystem.Models;
+using WeatherSystem.Services;
+using WeatherSystem.Strategies;
 
-try
+while (true)
 {
-    var x = ConfigRepository.Instance.BotsConfig;
-    Console.WriteLine(x[0].Name);
-}
-catch (Exception ex)
-{
-    Console.WriteLine(ex.Message);
+    Console.WriteLine("Weather monitoring and reporting service\n");
+    Console.WriteLine("Enter weather state using one of the following formats:");
+    Console.WriteLine("1. JSON Format");
+    Console.WriteLine("2. XML Format");
+    Console.Write("\nChoose an option: ");
+    var inputOption = Console.ReadLine();
+
+    if (!int.TryParse(inputOption, out var option) || option != 1 && option != 2)
+    {
+        Console.WriteLine("Invalid option");
+        goto repeat;
+    }
+
+    var inputStrategy = new WeatherStrategyContext();
+
+    if (option == 1)
+        inputStrategy.SetStrategy(new JsonWeatherInputStrategy());
+    else
+        inputStrategy.SetStrategy(new XmlWeatherInputStrategy());
+
+    Console.WriteLine("Enter the weather state object:");
+    var weatherStateInput = Console.ReadLine();
+    WeatherStateDTO weatherState = new();
+
+    try
+    {
+        weatherState = inputStrategy.WeatherDeserialize(weatherStateInput);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($"Invalid input:\n{e.Message}");
+        goto repeat;
+    }
+
+    BotsObserver.NotifySubscribers(weatherState);
+
+    BotsService.GetTriggeredWeatherBots()
+        .ForEach(bot => {
+                Console.WriteLine($"{bot.Name} activated!");
+                Console.WriteLine($"{bot.Name}: \"{bot.Message}\"\n");
+            }
+        );
+
+repeat:
+    Console.WriteLine("Press enter to continue...");
+    Console.ReadLine();
+    Console.Clear();
 }
