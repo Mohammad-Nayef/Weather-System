18ee39b670cd2c35bac50974d4f3dd6517b9d65e
mohammad-nayef <awladmohammad20@gmail.com>
2023-08-29 13:33:31 +0300

    Added summary for NotifySubscribers method

--- BotsObserver.cs
+++ BotsObserver.cs
@@ -10,6 +10,9 @@ namespace WeatherSystem
             get => BotsService.EnabledWeatherBots;
         }
 
+        /// <summary>
+        /// Activates specific bots based on the current weather state.
+        /// </summary>
         public static void NotifySubscribers(WeatherStateDTO weatherState)
         {
             Subscribers.ForEach(subscriber => subscriber.Update(weatherState));
